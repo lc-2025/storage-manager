d04787cc37f943ca42179bcecfaf6a04706aacdb
the-speaker3 <the_speaker3@protonmail.com>
2025-09-28 18:13:06 +0200

    feat: Storage manager implementation

---
+++ src/hooks/Storage.ts
@@ -0,0 +1,66 @@
+import { Storage } from '@/types/hooks/Storage.ts';
+import type { TUseStorage } from '@/types/hooks/Storage.ts';
+
+/**
+ * @description Storage hook
+ * @author Luca Cattide
+ * @date 28/09/2025
+ * @param {string} type
+ * @returns {*}  {TUseStorage}
+ */
+const useStorage = (type: string): TUseStorage => {
+  const storageType = {
+    [Storage.Local]: localStorage,
+    [Storage.Session]: sessionStorage,
+  };
+  const storage = storageType[type as keyof typeof storageType];
+
+  /**
+   * @description Storage getter
+   * Retrieves an item from Local/Session Storage if present
+   * @author Luca Cattide
+   * @param {string} item
+   * @returns {*}  {(string | null)}
+   */
+  const getStorage = (item: string): string | null => {
+    let element = null;
+
+    if (window[type as keyof typeof window]) {
+      element = storage.getItem(item);
+    }
+
+    return element;
+  };
+
+  /**
+   * @description Storage setter
+   * Saves an item to the Local/Session Storage
+   * @author Luca Cattide
+   * @param {string} item
+   * @param {string} value
+   */
+  const setStorage = (item: string, value: string): void => {
+    if (window[type as keyof typeof window]) {
+      storage.setItem(item, value);
+    }
+  };
+
+  /**
+   * @description Storage removal
+   * Deletes one or multiple items from the Local/Session Storage
+   * @author Luca Cattide
+   * @date 19/07/2025
+   * @param {Array<string>} items
+   */
+  const deleteStorages = (items: Array<string>): void => {
+    if (window[type as keyof typeof window]) {
+      items.forEach((item) => {
+        storage.removeItem(item);
+      });
+    }
+  };
+
+  return { getStorage, setStorage, deleteStorages };
+};
+
+export default useStorage;
