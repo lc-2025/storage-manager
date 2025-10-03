a6086ba78ade97be77ef3106edfbbd42467acb9f
the-speaker3 <the_speaker3@protonmail.com>
2025-10-03 09:58:20 +0200

    chore: CD pipeline update

---
+++ release/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+UPDATE=`git diff HEAD^ HEAD -- package.json | grep '^+ '`
+DEPLOY=false
+
+if [[ $UPDATE != *"version"* ]]; then
+  echo "Version bumped - Deployment allowed."
+
+  DEPLOY=true
+elif
+  echo "No version bumped."
+fi
+
+echo "$DEPLOY"
